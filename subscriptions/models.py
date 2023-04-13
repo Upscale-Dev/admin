2ee51592bca972dcc6d2bf25eb655bd99a1e3a6c
nofamex <nsiren517@gmail.com>
2023-04-14 03:37:33 +0700

    add status choices

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -8,7 +8,7 @@ STATUS_CHOICES = [
 class Subscriptions(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey('user.Users', models.CASCADE, blank=True, null=True)
-    status = models.TextField(blank=True, null=True)
+    status = models.TextField(blank=True, null=True, choices=STATUS_CHOICES)
     length = models.BigIntegerField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
