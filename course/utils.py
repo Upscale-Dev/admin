4ba971a7fa7e7894d84f62de76099ea6baa80732
tiketdatahaikal <muhammad.susanto@tiket.com>
2023-05-29 01:19:34 +0700

    create helper function to upload thumbnail and upload trailer

--- course/utils.py
+++ course/utils.py
@@ -1,5 +1,24 @@
-from datetime import datetime
 import pytz
+from datetime import datetime
+
+from course.custom_storage import ThumbnailStorage, TrailerStorage
+
+
+
+def upload_thumbnail(thumbnail_image):
+    thumbnail_storage = ThumbnailStorage()
+    thumbnail_image_name = thumbnail_image.__str__()
+    thumbnail_storage.save(thumbnail_image_name,thumbnail_image)
+    new_image_url = thumbnail_storage.url(thumbnail_image_name)
+    return new_image_url
+
+def upload_trailer(trailer_video):
+    trailer_storage  = TrailerStorage()
+    trailer_video_name = trailer_video.__str__()
+    trailer_storage.save(trailer_video_name, trailer_video)
+    new_trailer_url = trailer_storage.url(trailer_video_name)
+    return new_trailer_url
+
 
 def jkt_now():
     tz_jkt = pytz.timezone('Asia/Jakarta') 
