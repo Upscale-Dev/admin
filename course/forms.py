3212f648264c1c02959f0e35bb5cd6b955a55928
tiketdatahaikal <muhammad.susanto@tiket.com>
2023-05-30 00:32:23 +0700

    rewrite CourseAdminForm class to use proper upload proper function

--- course/forms.py
+++ course/forms.py
@@ -4,13 +4,14 @@ from django import forms
 from course import utils
 from course.models import Courses
 
+
 class CourseAdminForm(forms.ModelForm):
-    thumbnail_image = forms.FileField(label="Thumbnail Image", required=True)
+    image_file = forms.FileField(label="Image File", required=True)
     trailer_video = forms.FileField(label="Trailer Video", required=True)
 
     def save(self, commit=True) -> Any:
-        thumbnail_image = self.cleaned_data.get('thumbnail_image', None)
-        new_image_url = utils.upload_thumbnail(thumbnail_image)
+        image_file = self.cleaned_data.get('image_file', None)
+        new_image_url = utils.upload_image(image_file)
 
         trailer_video = self.cleaned_data.get('trailer_video', None)
         new_trailer_url = utils.upload_trailer(trailer_video)
@@ -19,10 +20,10 @@ class CourseAdminForm(forms.ModelForm):
         self.instance.trailer_url = new_trailer_url
         self.instance.created_at = utils.jkt_now()
         self.instance.updated_at = utils.jkt_now()
-        
-        return super().save(commit)
 
+        return super().save(commit)
 
     class Meta:
         model = Courses
-        exclude = ["image_url", "trailer_url", "updated_at", "created_at", "deleted_at"]
+        exclude = ["image_url", "trailer_url",
+                   "updated_at", "created_at", "deleted_at"]
