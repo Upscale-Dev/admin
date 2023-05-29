d0a78f49421981d7a315dc64646da3c5ed444a84
tiketdatahaikal <muhammad.susanto@tiket.com>
2023-05-30 00:25:35 +0700

    add ImageStorage class

--- course/custom_storage.py
+++ course/custom_storage.py
@@ -1,10 +1,17 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
+class ImageStorage(S3Boto3Storage):
+    bucket_name = "images"
+
+
 class ThumbnailStorage(S3Boto3Storage):
     bucket_name = "thumbnails"
 
+
 class TrailerStorage(S3Boto3Storage):
     bucket_name = "trailers"
+
 
 class VideoStorage(S3Boto3Storage):
     bucket_name = "videos"
